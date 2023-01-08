cae4b2e19ff08811d8253de54ee275bb46670198
abayomi <olasunkami010@gmail.com>
2023-01-08 02:15:01 -0600

    Head

--- GetHead.py
+++ GetHead.py
@@ -1,15 +1,19 @@
-import  socket                        
+import  socket
+s= socket.socket()                        
 
-target = 'packthub.samsclass.info'
+target = 'packtpub.samsclass.info' 
 port = input("Port Number:")
 try:
         
-        s.connect((target, port))
+        s.connect((target, int(port)))
         req = 'HEAD / HTTP/1.1\r\nHost: '+ target+'\r\n\r\n'
-        print('Sending')
+        print('Sending.......')
         print(req)
-        s.send(req)
+        a = req.encode('utf-8')
+        s.send(a)
+       
         print(s.recv(1024))
+        print(".....Received!! \n Done")
         s.close()
 except socket.error as err:
         print(err)
